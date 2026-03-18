1a27807fd498c5dbcaa575be099a7183c2f2c2e4
TAPCarlosAPL <carlos.lamounier@sempreceub.com>
2026-03-18 16:44:30 -0300

    Implementando mais casos de teste ao projeto

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 def soma(a,b):
      return a + b
+     
+def subtracao(a,b):
+     return a - b
 
      if __name__ == "__main__":
         print("Soma de 2 + 3 =", soma(2,3))
+        print("Subtração de 3 - 3 =", subtracao(3, 3))
--- test_app.py
+++ test_app.py
@@ -1,5 +1,26 @@
-from app import soma
+from app import soma, subtracao
 
 def test_soma():
     assert soma (2,3) == 5
-    assert soma (-1,1) == 0
+
+def test_subtracao():
+    assert subtracao (3,3) == 0
+
+#TESTANDO FUNÇÃO DE SUBTRACAO COM NÚMEROS NEGATIVOS
+def test_subtracao_negativo():
+    assert subtracao (-3,3) == -6
+    assert subtracao (-3,-3) == 0
+    assert subtracao (3,-3) == 6
+    assert subtracao(0, -3) == 3
+    assert subtracao(-3, 0) == -3
+
+#TESTANDO FUNÇÃO DE SOMA COM NÚMEROS NEGATIVOS
+def test_soma_negativos():
+    assert soma (-2,3) == 1
+    assert soma (-2,-3) == -5
+    assert soma (2,-3) == -1
+    assert soma (0, -3) == -3
+
+
+def test_erro_simulado():
+    assert soma(2, 3) == 10, "Erro simulado para testar o pipeline!!"
